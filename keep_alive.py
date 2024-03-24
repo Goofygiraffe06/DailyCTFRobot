026ffdf5a4012794f638674330edea730455bdca
Goofygiraffe06 <rahul.narsing06@gmail.com>
2024-03-24 16:36:08 +0530

    Changing port from 8080 to 1337 changing from port 8080 to 1337 because it is being used by some other program probably my proxy server :(

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def home():
     return "<b>Hack The Planet</b>"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  app.run(host='0.0.0.0',port=1337) # Change it to 8080 or anything you like
 
 def keep_alive():
     t = Thread(target=run)
